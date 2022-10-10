3481658e8e240f3f7264591f72e0f968bd715c02
sartajdev007 <sartaj5akik@gmail.com>
2022-10-11 02:51:24 +0600

    header links style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -10,9 +10,21 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='topics'>Topics</Link></li>
-                        <li><Link to='statistics'>Statistics</Link></li>
-                        <li><Link to='blogs'>Blogs</Link></li>
+                        <li><NavLink to='topics' className={({ isActive }) =>
+                            isActive
+                                ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                        }>Topics</NavLink></li>
+                        <li><NavLink to='statistics' className={({ isActive }) =>
+                            isActive
+                                ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                        }>Statistics</NavLink></li>
+                        <li><NavLink to='blogs' className={({ isActive }) =>
+                            isActive
+                                ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                        }>Blogs</NavLink></li>
                     </ul>
                 </div>
                 <button className='btn btn-ghost normal-case text-xl'>
@@ -21,9 +33,21 @@ const Header = () => {
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className='menu menu-horizontal p-0'>
-                    <li><Link to='topics'>Topics</Link></li>
-                    <li><Link to='statistics'>Statistics</Link></li>
-                    <li><Link to='blogs'>Blogs</Link></li>
+                    <li><NavLink to='topics' className={({ isActive }) =>
+                        isActive
+                            ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                            : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                    }>Topics</NavLink></li>
+                    <li><NavLink to='statistics' className={({ isActive }) =>
+                        isActive
+                            ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                            : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                    }>Statistics</NavLink></li>
+                    <li><NavLink to='blogs' className={({ isActive }) =>
+                        isActive
+                            ? 'font-medium tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                            : 'font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                    }>Blogs</NavLink></li>
                 </ul>
             </div>
         </div>
