76929c44eec84fbf8cddf1b798cae36927c0fba5
sartajdev007 <sartaj5akik@gmail.com>
2022-10-11 10:53:10 +0600

    readme add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
